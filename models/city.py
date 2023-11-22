85d0b7f911bae75355dc2f015fa079323b12f5c3
UmarMsani <umuhammad220@gmail.com>
2023-11-22 08:52:22 -0800

    AirBnB clone - MySQL

--- models/city.py
+++ models/city.py
@@ -1,24 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-class City(BaseModel, Base):
+
+class City(BaseModel):
     """ The city class, contains state ID and name """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
-
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
-
-    else:
-        name = ""
-        state_id = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
+    state_id = ""
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import models
-from os import getenv
 
-class State(BaseModel, Base):
+
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
-
-    else:
-        name = ""
-
-    @property
-    def cities(self):
-        """Getter attribute to retrieve cities for this state"""
-        from models import storage
-        city_instances = storage.all('City')
-        return [city for city in city_instances.values() if city.state_id == self.id]
+    name = ""
