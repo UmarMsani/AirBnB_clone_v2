7e66e9e780684cd34c5defe6c89c1a54fc5c7749
UmarMsani <umuhammad220@gmail.com>
2023-12-12 17:59:34 -0800

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,46 +2,28 @@
 # Script for setting up web servers for web_static deployment
 
 # Install Nginx if not installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Create necessary folders if they don't exist
-folders=("/data" "/data/web_static" "/data/web_static/releases" "/data/web_static/shared" "/data/web_static/releases/test")
-for folder in "${folders[@]}"; do
-    if [ ! -d "$folder" ]; then
-        sudo mkdir -p "$folder"
-    fi
-done
-
-# Change ownership specifically for /data
-sudo chown -R ubuntu:ubuntu "/data"
-
-# Create fake HTML file for testing
-html_content="<html>
-<head>
-    <title>Test Page</title>
-</head>
-<body>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
     Testing Nginx configuration
-</body>
-</html>"
-html_path="/data/web_static/releases/test/index.html"
-echo "$html_content" | sudo tee "$html_path" > /dev/null
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or recreate symbolic link
-symbolic_link="/data/web_static/current"
-if [ -L "$symbolic_link" ]; then
-    sudo rm "$symbolic_link"
-fi
-sudo ln -s /data/web_static/releases/test/ "$symbolic_link"
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Update Nginx configuration
-nginx_config="/etc/nginx/sites-enabled/default"
-sudo sed -i '/hbnb_static/ d' "$nginx_config"
-nginx_content="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "/server_name _;/a $nginx_content" "$nginx_config"
+sudo chown -R ubuntu:ubuntu /data/
 
-# Restart Nginx
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
